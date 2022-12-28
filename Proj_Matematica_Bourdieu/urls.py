922a1d853b837d1c5ec332555ea05ad3ad73d741
Thurcavalcante <arthurcavalcantethur@gmail.com>
2022-12-28 11:09:34 -0300

    Adicionadas as páginas: Home, Cadastro e noticias

--- Proj_Matematica_Bourdieu/urls.py
+++ Proj_Matematica_Bourdieu/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core.views import home, cadastro, noticias
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home'),
+    # path('login/', login, name='login'),
+    path('cadastro/', cadastro, name='cadastro'),
+    path('noticias/', noticias, name='noticias'),
 ]
