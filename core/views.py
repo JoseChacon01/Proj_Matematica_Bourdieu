db9c85ce612b75bafd1c47f88d5459c22c67b7f5
pedro_lucas <p.leopoldino03@gmail.com>
2023-01-14 17:46:47 -0300

    views

--- core/views.py
+++ core/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+def home (resquest):
+    return render(resquest, "index.html")
+
+def cadastro(resquest):
+    return render(resquest, "cadstro.html")
+
+def noticias(resquest):
+    return render(resquest, "noticias.html") 
 # Create your views here.
