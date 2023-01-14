db5a341f6b6da8979b57c9bbfb1cde2326ebcb4c
pedro_lucas <p.leopoldino03@gmail.com>
2023-01-14 16:45:51 -0300

    novasAt

--- Proj_Matematica_Bourdieu/urls.py
+++ Proj_Matematica_Bourdieu/urls.py
@@ -21,6 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
     # path('login/', login, name='login'),
-    path('cadastro/', cadastro, name='cadastro'),
-    path('noticias/', noticias, name='noticias'),
+    # path('cadastro/', cadastro, name='cadastro'),
+    # path('noticias/', noticias, name='noticias'),
 ]
